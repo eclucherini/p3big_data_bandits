5fe079fe2f05d524a8250f726cfb4123bb747bb7
ParedesA <andreaparedesbr@gmail.com>
2023-07-31 02:18:58 -0700

    updated files

--- Resources/AP/APcopy.js
+++ Resources/AP/APcopy.js
@@ -1,35 +1,81 @@
-// Top 10 hosts with the most listing
-
-let hosts = ["The Box House Hotel ", "Tony ", "Seith ", "Jean ","Jade ","Vie ", "Vasili ","Henry ","Christian & Carla ", "Marylyn "]
-
-let count = [40, 37, 33, 26, 23, 19, 19, 17, 13, 13]
-
-    // Function to generate color based on the count
-    function getColor(countValue) {
-        // Define your color logic here
-        // For example, you can set different colors based on count ranges
-        if (countValue >= 30) {
-          return 'rgba(54, 162, 235, 0.6)';
-        } else if (countValue >= 20) {
-          return 'rgba(255, 99, 132, 0.6)';
-        } else {
-          return 'rgba(75, 192, 192, 0.6)';
-        }
-      }
+// Fetch data from the URL and create a bar chart for the top 10 hosts with the most listings.
+d3.json("http://127.0.0.1:5000/api/BigDataBandits/Listings").then(function (data) {
+  // Create an object to store the count of listings for each host
+  let uniqueHostNames = [];
+  let totalListingsCountArray = [];
   
-      let trace1 = {
-        x: count,
-        y: hosts,
-        type: 'bar',
-        orientation: 'h',
-        marker: {
-          color: count.map(countValue => getColor(countValue))
-        }
-      };
+  // Loop through the data and populate the arrays
+  data.forEach(function (listing) {
+    const hostName = listing.host_name;
+    const totalListingsCount = listing.host_total_listings_count;
+  
+    // Check if the host_name is not already in the uniqueHostNames array
+    if (!uniqueHostNames.includes(hostName)) {
+      uniqueHostNames.push(hostName); // Add the host_name to the uniqueHostNames array
+      totalListingsCountArray.push(totalListingsCount); // Add the corresponding total listings count to the totalListingsCountArray
+    } else {
+      // If the host_name is already in the uniqueHostNames array, find its index
+      const index = uniqueHostNames.indexOf(hostName);  
+    }
+  });
 
-let data = [trace1];
+  const hostListingsDict = uniqueHostNames.reduce((result, hostName, index) => {
+    result[hostName] = totalListingsCountArray[index];
+    return result;
+  }, {});
 
-let layout = {
+  //Transforme the dictionary into an array  
+  const hostListingsArray = Object.entries(hostListingsDict);
+
+  // Sort the array in descending order based on the values
+  hostListingsArray.sort((a, b) => b[1] - a[1]);
+  
+  // Slicing to keep only top 10
+  let top10Hosts = hostListingsArray.slice(0, 10);
+
+  //Separating array into 2 different arrays
+  var keylist = [];
+  var valuelist = [];
+
+  for (let i = 0; i < top10Hosts.length; i++) {
+    keylist.push(top10Hosts[i][0]); // Push the host name (key) at index 0
+    valuelist.push(top10Hosts[i][1]); // Push the total listings count (value) at index 1
+  }
+  
+  console.log(keylist);
+  console.log(valuelist);
+
+  // Function to generate color based on the count
+  function getColor(countValue) {
+    // Define your color logic here
+    // For example, you can set different colors based on count ranges
+    if (countValue >= 30) {
+      return 'rgba(54, 162, 235, 0.6)';
+    } else if (countValue >= 20) {
+      return 'rgba(255, 99, 132, 0.6)';
+    } else {
+      return 'rgba(75, 192, 192, 0.6)';
+    }
+  }
+
+  //reversing arrays to show host with the most listings on top
+  keylist = keylist.reverse();
+  valuelist = valuelist.reverse();
+  
+  //creating chart
+  let trace1 = {
+    x: valuelist,
+    y: keylist,
+    type: 'bar',
+    orientation: 'h',
+    marker: {
+      color: valuelist.map(countValue => getColor(countValue))
+    }
+  };
+
+  let data1 = [trace1];
+
+  let layout = {
     title: {
       text: 'Number of Listings per Hosts - Top 10',
       font: {
@@ -42,7 +88,13 @@ let layout = {
       family: 'Arial, sans-serif',
       size: 14,
       color: 'black'
+    },
+    yaxis: {
+      automargin: true // Adjust margins to accommodate full labels
     }
   };
 
-Plotly.newPlot("plot", data, layout);
+  Plotly.newPlot("plot", data1, layout);
+}).catch(function (error) {
+  console.log("Error fetching data:", error);
+});
--- Resources/AP/APmap.js
+++ Resources/AP/APmap.js
@@ -1,87 +1,84 @@
-// Create a map object.
-// let myMap = L.map("map", {
-//   center: [37.09, -95.71],
-//   zoom: 5
-// });
+function markerSize(listings) {
+  return Math.sqrt(listings) * 20;
+}
 
-let myMap = L.map("map", {
+// Fetch data from the URL and create markers based on unique hosts.
+d3.json("http://127.0.0.1:5000/api/BigDataBandits/Listings").then(function (data) {
+  // Create an object to store unique hosts based on host_id
+  let uniqueHosts = {};
+
+  // Loop through the data and populate the uniqueHosts object
+  data.forEach(function (listing) {
+    const host_id = listing.host_id;
+    if (!uniqueHosts.hasOwnProperty(host_id)) {
+      uniqueHosts[host_id] = {
+        name: listing.host_name,
+        location: [listing.latitude, listing.longitude],
+        listings: listing.host_total_listings_count,
+        url: listing.host_url
+      };
+    }
+  });
+
+  // Convert the uniqueHosts object into an array of hosts
+  let hosts = Array.from(Object.values(uniqueHosts));
+
+  let sortedhosts = hosts.sort((a, b) => b.listings - a.listings);
+
+  // Slicing to keep only top 10
+  let top10hosts = sortedhosts.slice(0, 10);
+
+  // Call the createMap function with top10hosts data
+  createMap(top10hosts);
+}).catch(function (error) {
+  console.log("Error fetching data:", error);
+});
+
+function createMap(topHosts) {
+  // Create the tile layer that will be the background of our map.
+  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // Create a baseMaps object to hold the streetmap layer.
+  let baseMaps = {
+    "Street Map": streetmap
+  };
+
+  // Create an overlayMaps object to hold the topHosts layer.
+  let overlayMaps = {
+    "Top Hosts": L.layerGroup() // Create an empty layerGroup for now
+  };
+
+  let myMap = L.map("map-id", {
     center: [40.7128, -74.0060], // New York coordinates
-    zoom: 11 // Adjust the zoom level as needed
+    zoom: 11, // Adjust the zoom level as needed
+    layers: [streetmap] // Only add the streetmap for now
+  });
+
+  // Loop through the topHosts array, and create one marker for each host object.
+  const customIcon = L.divIcon({
+    className: 'custom-icon',
+    html: '<div style="background-color: purple; border: 2px solid white; border-radius: 50%; width: 20px; height: 20px;"></div>',
   });
   
-  // Add a tile layer.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  // Define a markerSize() function that will give each city a different radius based on its population.
-  function markerSize(listings) {
-    return Math.sqrt(listings) * 0.5;
-  }
-  
-  // Each city object contains the city's name, location, and population.
-  // Population Data Source: U.S. 2020 Decennial Census
-  let hosts = [
-    {
-      name: "The Box House Hotel",
-      location: [40.73777, -73.95366],
-      listings: 40
-    },
-    {
-      name: "Tony",
-      location: [40.72021, -73.99426],
-      listings: 37
-    },
-    {
-      name: "Seith",
-      location: [40.72641, -73.98711],
-      listings: 33
-    },
-    {
-      name: "Jean",
-      location: [40.7106, -73.85246],
-      listings: 26
-    },
-    {
-      name: "Jade",
-      location: [40.7209818, -73.9931495],
-      listings: 23
-    },
-    {
-      name: "Vasili",
-      location: [40.772472, -73.906075],
-      listings: 19
-    },
-    {
-      name: "Vie",
-      location: [40.881, -73.83511],
-      listings: 19
-    },
-    {
-      name: "Henry",
-      location: [40.7751, -73.98595],
-      listings: 17
-    },
-    {
-      name: "Christian & Carla",
-      location: [40.86502, -73.85496],
-      listings: 13
-    },
-    {
-      name: "Marylyn Palmer",
-      location: [40.67823, -73.94719],
-      listings: 13
-    }
-  ];
-  
-  // Loop through the cities array, and create one marker for each city object.
-  for (let i = 0; i < hosts.length; i++) {
-    L.marker(hosts[i].location, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: "purple",
+  // Loop through the top10hosts array, and create one marker for each host object.
+  for (let i = 0; i < topHosts.length; i++) {
+    L.marker(topHosts[i].location, {
+      icon: customIcon,
       // Setting our circle's radius to equal the output of our markerSize() function:
-      // This will make our marker's size proportionate to its population.
-      radius: markerSize(hosts[i].listings)
-    }).bindPopup(`<h1>${hosts[i].name}</h1> <hr> <h3>listings: ${hosts[i].listings.toLocaleString()}</h3>`).addTo(myMap);
+      // This will make our marker's size proportionate to its listings count.
+      radius: markerSize(topHosts[i].listings)
+    }).bindPopup(`<h1 style="color: purple; line-height: 0.5; margin-bottom: 3px;">${topHosts[i].name}</h1>
+                <p><a href="${topHosts[i].url}" target="_blank">Host Profile</a></p>
+                <hr>
+                <h3 style="color: purple; line-height: 0.5; margin-bottom: 3px;">Total Listings: ${topHosts[i].listings.toLocaleString()}</h3>`).addTo(myMap);
   }
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  // Now that the markers are added, add the overlayMaps layerGroup to the map
+  overlayMaps["Top Hosts"].addTo(myMap);
+};
