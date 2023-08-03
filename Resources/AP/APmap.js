88e039acb729cae54acff8f4aa137c153b4fe960
ParedesA <128671718+ParedesA@users.noreply.github.com>
2023-08-03 15:54:36 -0700

    Update APmap.js
    Clean up

--- Resources/AP/APmap.js
+++ Resources/AP/APmap.js
@@ -2,12 +2,12 @@ function markerSize(listings) {
   return Math.sqrt(listings) * 20;
 }
 
-// Fetch data from the URL and create markers based on unique hosts.
+// Fetching data from the URL
 d3.json("http://127.0.0.1:5000/api/BigDataBandits/Listings").then(function (data) {
-  // Create an object to store unique hosts based on host_id
+  // Object to store unique hosts
   let uniqueHosts = {};
 
-  // Loop through the data and populate the uniqueHosts object
+  // Loop to populate uniqueHosts
   data.forEach(function (listing) {
     const host_id = listing.host_id;
     if (!uniqueHosts.hasOwnProperty(host_id)) {
@@ -20,7 +20,7 @@ d3.json("http://127.0.0.1:5000/api/BigDataBandits/Listings").then(function (data
     }
   });
 
-  // Convert the uniqueHosts object into an array of hosts
+  // Converting the uniqueHosts object into an array of hosts
   let hosts = Array.from(Object.values(uniqueHosts));
 
   let sortedhosts = hosts.sort((a, b) => b.listings - a.listings);
@@ -28,27 +28,27 @@ d3.json("http://127.0.0.1:5000/api/BigDataBandits/Listings").then(function (data
   // Slicing to keep only top 10
   let top10hosts = sortedhosts.slice(0, 10);
 
-  // Call the createMap function with top10hosts data
+  // CreateMap function
   createMap(top10hosts);
 }).catch(function (error) {
   console.log("Error fetching data:", error);
 });
 
 function createMap(topHosts) {
-  // Create the tile layer that will be the background of our map.
+  
   let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
 
-  // Create a baseMaps object to hold the streetmap layer.
+  // Creating a baseMaps object to hold the streetmap layer.
   let baseMaps = {
     "Street Map": streetmap
   };
 
-  // Create an empty layerGroup to hold the topHosts layer.
+  // Creating an empty layerGroup to hold the topHosts layer.
   let tophostlayer = L.layerGroup();
 
-  // Loop through the topHosts array, and create one marker for each host object.
+  // Loop to create markers
   const customIcon = L.divIcon({
     className: 'custom-icon',
     html: '<div style="background-color: purple; border: 2px solid white; border-radius: 50%; width: 20px; height: 20px;"></div>',
@@ -65,8 +65,8 @@ function createMap(topHosts) {
   }
 
   let myMap = L.map("map-id", {
-    center: [40.730610, -73.935242], // New York coordinates
-    zoom: 11, // Adjust the zoom level as needed
+    center: [40.730610, -73.935242],
+    zoom: 11,
     layers: [streetmap, tophostlayer]
   });
 
